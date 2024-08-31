81795c2d251403eb119e72b2712a14812bb12465
Gavin Ong <kukudoom@gmail.com>
2024-08-31 16:20:07 +0800

    first page changes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -24,11 +24,11 @@ export default async function Home({ searchParams }: SearchParamProps) {
       <section className="bg-primary-50 bg-dotted-pattern bg-contain py-5 md:py-10">
         <div className="wrapper grid grid-cols-1 gap-5 md:grid-cols-2 2xl:gap-0">
           <div className="flex flex-col justify-center gap-8">
-            <h1 className="h1-bold">Host, Connect, Celebrate: Your Events, Our Platform!</h1>
-            <p className="p-regular-20 md:p-regular-24">Book and learn helpful tips from 3,168+ mentors in world-class companies with our global community.</p>
+            <h1 className="h1-bold">阿弥陀佛是我们的救主，是我们真正平安、喜乐、希望的来源；在人世间有苦难，在佛怀中有平安。</h1>
+            <p className="p-regular-20 md:p-regular-24">南无阿弥陀佛</p>
             <Button size="lg" asChild className="button w-full sm:w-fit">
               <Link href="#events">
-                Explore Now
+                参观活动
               </Link>
             </Button>
           </div>
@@ -44,7 +44,7 @@ export default async function Home({ searchParams }: SearchParamProps) {
       </section> 
 
       <section id="events" className="wrapper my-8 flex flex-col gap-8 md:gap-12">
-        <h2 className="h2-bold">Trust by <br /> Thousands of Events</h2>
+        <h2 className="h2-bold">最新活动</h2>
 
         <div className="flex w-full flex-col gap-5 md:flex-row">
           <Search />
