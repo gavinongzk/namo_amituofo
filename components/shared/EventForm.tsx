4c339329ad1b0168764d16bbfae8ae1ce16bffde
Gavin Ong <kukudoom@gmail.com>
2024-09-01 11:41:29 +0800

    use customForm

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -21,6 +21,7 @@ import { Checkbox } from "../ui/checkbox"
 import { useRouter } from "next/navigation"
 import { createEvent, updateEvent } from "@/lib/actions/event.actions"
 import { IEvent } from "@/lib/database/models/event.model"
+import CustomFieldsPopup from "./CustomFieldsPopup"
 
 
 type EventFormProps = {
@@ -254,47 +255,6 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
         </div>
 
         <div className="flex flex-col gap-5 md:flex-row">
-            <FormField
-              control={form.control}
-              name="price"
-              render={({ field }) => (
-                <FormItem className="w-full">
-                  <FormControl>
-                    <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
-                      <Image
-                        src="/assets/icons/dollar.svg"
-                        alt="dollar"
-                        width={24}
-                        height={24}
-                        className="filter-grey"
-                      />
-                      <Input type="number" placeholder="Price" {...field} className="p-regular-16 border-0 bg-grey-50 outline-offset-0 focus:border-0 focus-visible:ring-0 focus-visible:ring-offset-0" />
-                      <FormField
-                        control={form.control}
-                        name="isFree"
-                        render={({ field }) => (
-                          <FormItem>
-                            <FormControl>
-                              <div className="flex items-center">
-                                <label htmlFor="isFree" className="whitespace-nowrap pr-3 leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">Free Ticket</label>
-                                <Checkbox
-                                  onCheckedChange={field.onChange}
-                                  checked={field.value}
-                                id="isFree" className="mr-2 h-5 w-5 border-2 border-primary-500" />
-                              </div>
-          
-                            </FormControl>
-                            <FormMessage />
-                          </FormItem>
-                        )}
-                      />   
-                    </div>
-
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />   
            <FormField
               control={form.control}
               name="url"
@@ -319,6 +279,21 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
             />
         </div>
 
+        <FormField
+          control={form.control}
+          name="customFields"
+          render={({ field }) => (
+            <FormItem className="w-full">
+              <FormControl>
+                <CustomFieldsPopup
+                  onSave={(fields) => field.onChange(fields)}
+                  initialFields={field.value}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
 
         <Button 
           type="submit"
