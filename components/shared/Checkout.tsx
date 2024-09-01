45d3b437dc372c39ecea4b0e7f7a0771975c64a7
Gavin Ong <kukudoom@gmail.com>
2024-09-01 10:47:59 +0800

    bugfix

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -26,20 +26,20 @@ const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
   return (
     <form onSubmit={(e) => { e.preventDefault(); onCheckout(); }} className="space-y-4">
       {event.customFields && event.customFields.map((field) => (
-        <div key={field.id}>
-          <label htmlFor={field.id} className="block text-sm font-medium text-gray-700">
+        <div key={field.id.toString()}>
+          <label htmlFor={field.id.toString()} className="block text-sm font-medium text-gray-700">
             {field.label}
           </label>
           {field.type === 'boolean' ? (
             <Checkbox
-              id={field.id}
+              id={field.id.toString()}
               checked={!!customFieldValues[field.id]}
               onCheckedChange={(checked) => handleCustomFieldChange(field.id, checked)}
             />
           ) : (
             <Input
               type={field.type}
-              id={field.id}
+              id={field.id.toString()}
               onChange={(e) => handleCustomFieldChange(field.id, e.target.value)}
               required
             />
--- components/shared/Footer.tsx
+++ components/shared/Footer.tsx
@@ -9,8 +9,8 @@ const Footer = () => {
           <Image 
             src="/assets/images/logo.svg"
             alt="logo"
-            width={128}
-            height={38}
+            width={64}
+            height={24}
           />
         </Link>
 
