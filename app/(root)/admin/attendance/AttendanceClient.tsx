e90338bc1dd4d2098274ed6b54383d3ee60a83b7
Gavin Ong <kukudoom@gmail.com>
2024-09-10 20:51:50 +0800

    debug

--- app/(root)/admin/attendance/AttendanceClient.tsx
+++ app/(root)/admin/attendance/AttendanceClient.tsx
@@ -112,7 +112,6 @@ const AttendanceClient = ({ event }: { event: Event }) => {
 
   return (
     <div className="wrapper my-8">
-      {console.log('AttendanceClient: Rendering component')}
       <div className="bg-white shadow-md rounded-lg p-6 mb-8">
         <h2 className="text-2xl font-bold mb-4">{event.title}</h2>
         <p className="text-gray-600 mb-2">
