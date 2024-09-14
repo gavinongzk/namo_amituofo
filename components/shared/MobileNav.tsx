5955ce7be7823c4b856d60383cad21a689153311
Gavin Ong <kukudoom@gmail.com>
2024-09-14 13:49:34 +0800

    tooltip changes

--- components/shared/MobileNav.tsx
+++ components/shared/MobileNav.tsx
@@ -2,6 +2,7 @@ import Link from "next/link"
 import Image from "next/image"
 import NavItems from "./NavItems"
 import { useUser } from "@clerk/nextjs"
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 
 const MobileNav = () => {
   const { user } = useUser();
@@ -11,7 +12,9 @@ const MobileNav = () => {
   return (
     <nav className="fixed bottom-0 left-0 right-0 bg-white shadow-md md:hidden">
       <div className="flex justify-around items-center py-2">
-        <NavItems isSuperAdmin={isSuperAdmin} isNormalAdmin={isNormalAdmin} />
+        <TooltipProvider>
+          <NavItems isSuperAdmin={isSuperAdmin} isNormalAdmin={isNormalAdmin} />
+        </TooltipProvider>
       </div>
     </nav>
   )
--- components/shared/NavItems.tsx
+++ components/shared/NavItems.tsx
@@ -3,29 +3,54 @@
 import Link from 'next/link';
 import Image from 'next/image';
 import React from 'react';
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip";
 
 const NavItems: React.FC<{ isSuperAdmin: boolean, isNormalAdmin: boolean }> = ({ isSuperAdmin, isNormalAdmin }) => {
   return (
     <>
-      <Link href="/" className="flex flex-col items-center">
-        <Image src="/assets/icons/home.png" alt="Home" width={24} height={24} />
-        <span className="text-xs mt-1">活动</span>
-      </Link>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href="/" className="flex flex-col items-center">
+            <Image src="/assets/icons/home.png" alt="Home" width={24} height={24} />
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>活动 Events</p>
+        </TooltipContent>
+      </Tooltip>
       {isSuperAdmin && (
-        <Link href="/events/create" className="flex flex-col items-center">
-          <Image src="/assets/icons/calendar.png" alt="Create" width={24} height={24} />
-          <span className="text-xs mt-1">创建</span>
-        </Link>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/events/create" className="flex flex-col items-center">
+              <Image src="/assets/icons/calendar.png" alt="Create" width={24} height={24} />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>创建 Create</p>
+          </TooltipContent>
+        </Tooltip>
       )}
-      <Link href="/profile" className="flex flex-col items-center">
-        <Image src="/assets/icons/ticket.png" alt="Ticket" width={24} height={24} />
-        <span className="text-xs mt-1">我的</span>
-      </Link>
+      <Tooltip>
+        <TooltipTrigger asChild>
+          <Link href="/profile" className="flex flex-col items-center">
+            <Image src="/assets/icons/ticket.png" alt="Ticket" width={24} height={24} />
+          </Link>
+        </TooltipTrigger>
+        <TooltipContent>
+          <p>我的 My Events</p>
+        </TooltipContent>
+      </Tooltip>
       {(isSuperAdmin || isNormalAdmin) && (
-        <Link href="/admin/dashboard" className="flex flex-col items-center">
-          <Image src="/assets/icons/admin.png" alt="Admin" width={24} height={24} />
-          <span className="text-xs mt-1">管理</span>
-        </Link>
+        <Tooltip>
+          <TooltipTrigger asChild>
+            <Link href="/admin/dashboard" className="flex flex-col items-center">
+              <Image src="/assets/icons/admin.png" alt="Admin" width={24} height={24} />
+            </Link>
+          </TooltipTrigger>
+          <TooltipContent>
+            <p>管理 Admin</p>
+          </TooltipContent>
+        </Tooltip>
       )}
     </>
   );
