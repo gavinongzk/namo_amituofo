bfea49e0119b466a3cab6ac25d10cc3d86f75d22
Gavin Ong <kukudoom@gmail.com>
2024-09-14 20:55:13 +0800

    classname

--- components/shared/NavWrapper.tsx
+++ components/shared/NavWrapper.tsx
@@ -1,12 +1,23 @@
-import { currentUser } from '@clerk/nextjs';
+import React from 'react';
+import { useUser } from '@clerk/nextjs';
 import NavItems from './NavItems';
 
-const NavWrapper = async () => {
-  const user = await currentUser();
-  const isSuperAdmin = user?.publicMetadata.role === 'superadmin';
-  const isNormalAdmin = user?.publicMetadata.role === 'admin';
+interface NavWrapperProps {
+  className?: string;
+}
 
-  return <NavItems isSuperAdmin={isSuperAdmin} isNormalAdmin={isNormalAdmin}/>;
+const NavWrapper: React.FC<NavWrapperProps> = ({ className }) => {
+  const { user } = useUser();
+  const isSuperAdmin = user?.publicMetadata?.role === 'superadmin';
+  const isNormalAdmin = user?.publicMetadata?.role === 'admin';
+
+  return (
+    <NavItems 
+      isSuperAdmin={isSuperAdmin} 
+      isNormalAdmin={isNormalAdmin} 
+      className={className}
+    />
+  );
 };
 
 export default NavWrapper;
