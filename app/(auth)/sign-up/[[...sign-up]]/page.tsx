926102e9e3a1bfa948ea5d271b05ebe7e281dda8
Gavin Ong <kukudoom@gmail.com>
2024-09-14 22:44:44 +0800

    revert sign-in signup

--- app/(auth)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.tsx
@@ -1,21 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
-import { useUser } from "@clerk/nextjs";
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
 
 export default function Page() {
-  const { isLoaded, isSignedIn } = useUser();
-  const router = useRouter();
-
-  useEffect(() => {
-    if (isLoaded && isSignedIn) {
-      router.push("/"); // Redirect to Home if signed in
-    }
-  }, [isLoaded, isSignedIn, router]);
-
-  if (!isLoaded || isSignedIn) {
-    return null; // Optionally, return a loading spinner
-  }
-
-  return <SignUp />;
+  return <SignUp />
 }
