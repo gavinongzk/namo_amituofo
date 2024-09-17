c1f6606b8b89d9402b6a62be054fabcf4c3099b7
Gavin Ong <kukudoom@gmail.com>
2024-09-17 21:50:17 +0800

    try middleware

--- middleware.ts
+++ middleware.ts
@@ -1,29 +1,24 @@
 import { authMiddleware } from "@clerk/nextjs";
-import { NextRequest, NextResponse } from 'next/server';
-import { currentUser } from '@clerk/nextjs';
+import { NextResponse } from 'next/server';
 
 export default authMiddleware({
   publicRoutes: [
     '/',
     '/events/:id',
     '/api/webhook/clerk',
-    '/api/uploadthing',
-    '/api/events'
+    '/api/events',
   ],
   ignoredRoutes: [
     '/api/webhook/clerk',
-    '/api/uploadthing'
+    '/api/uploadthing',
   ],
   async afterAuth(auth, req) {
-    // Custom logic after Clerk's authentication
     if (auth.isPublicRoute) {
       return NextResponse.next();
     }
 
-    const user = await currentUser();
-    const role = user?.publicMetadata.role as string;
+    const role = auth.sessionClaims?.role as string;
 
-    // Check if the route requires specific roles
     if (req.nextUrl.pathname.startsWith('/api/')) {
       const allowedRoles = getAllowedRoles(req.nextUrl.pathname);
       if (!allowedRoles.includes(role)) {
@@ -43,11 +38,9 @@ export const config = {
 };
 
 function getAllowedRoles(pathname: string): string[] {
-  // Define your route-role mappings here
   const routeRoles: { [key: string]: string[] } = {
     '/api/users/update-role': ['superadmin'],
     '/api/attendance': ['superadmin', 'admin'],
-    // Add more route-role mappings as needed
   };
 
   return routeRoles[pathname] || ['user', 'admin', 'superadmin'];
