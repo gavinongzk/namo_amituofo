a7fb8c4c456efe86ec8e593122caa470e9fbd68a
Gavin Ong <kukudoom@gmail.com>
2024-09-27 15:22:32 +0800

    link to order page

--- components/shared/RegistrationCard.tsx
+++ components/shared/RegistrationCard.tsx
@@ -32,7 +32,7 @@ const RegistrationCard: React.FC<CardProps> = ({ event, registrations, isMyTicke
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link 
-        href={isMyTicket ? `/orders/${event.orderId}` : `/events/${event._id}`} // Conditional redirect
+        href={`/orders/${event.orderId}`}
         className="flex-center aspect-square w-full bg-gray-50 bg-cover bg-center text-grey-500"
         style={{backgroundImage: `url(${event.imageUrl})`}}
       />
@@ -40,18 +40,14 @@ const RegistrationCard: React.FC<CardProps> = ({ event, registrations, isMyTicke
         <h3 className="text-xl font-bold text-gray-800">{event.title}</h3>
         <ul className="mt-4 space-y-2">
           {registrations.map((reg, index) => (
-            <li key={index} className="flex flex-col py-1 text-gray-600">
-              <div className="flex justify-between">
-                <span>Queue Number 队列号码: <span className="font-medium">{reg.queueNumber}</span></span>
-              </div>
-              <div className="flex justify-between">
-                <span>Name 姓名: <span className="font-medium">{reg.name}</span></span>
-              </div>
+            <li key={index} className="flex justify-between py-1 text-gray-600">
+              <span>Queue Number: <span className="font-medium">{reg.queueNumber}</span></span>
+              <span>Name: <span className="font-medium">{reg.name}</span></span>
             </li>
           ))}
         </ul>
         <Button asChild size="sm" className="mt-4 self-start">
-          <Link href={`/orders/${event.orderId}`}>View Order Details 查看订单详情</Link>
+          <Link href={`/orders/${event.orderId}`}>View Order Details</Link>
         </Button>
       </div>
     </div>
