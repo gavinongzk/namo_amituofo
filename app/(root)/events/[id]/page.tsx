0ee41f5826d8556cef5f0fb7543109ce2056a8c8
Gavin Ong <kukudoom@gmail.com>
2024-09-30 15:08:47 +0800

    image at the top

--- app/(root)/events/[id]/page.tsx
+++ app/(root)/events/[id]/page.tsx
@@ -18,7 +18,7 @@ const EventDetails = async ({ params: { id }, searchParams }: SearchParamProps) 
     <>
     <section className="flex justify-center bg-primary-50 bg-dotted-pattern bg-contain">
       <div className="grid grid-cols-1 md:grid-cols-2 2xl:max-w-7xl w-full">
-        <div className="flex items-center justify-center p-5 md:p-10">
+        <div className="flex items-start justify-center p-5 md:p-10 md:sticky md:top-0">
           <Image 
             src={event.imageUrl}
             alt={event.title}
