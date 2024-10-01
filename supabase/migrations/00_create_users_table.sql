c514e364b9801f55de624b0d6c58cf82496f14b8
Gavin Ong <kukudoom@gmail.com>
2024-10-01 15:41:52 +0800

    removed supabase

--- supabase/migrations/00_create_users_table.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE users (
-  id UUID DEFAULT uuid_generate_v4() PRIMARY KEY,
-  clerk_id TEXT UNIQUE NOT NULL,
-  email TEXT,
-  phone_number TEXT,
-  role TEXT DEFAULT 'user' CHECK (role IN ('superadmin', 'admin', 'user'))
-);
