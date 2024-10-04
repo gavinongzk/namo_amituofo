66c882af44457dde1cca93dfcd4a3d1982c8b330
Gavin Ong <kukudoom@gmail.com>
2024-10-04 13:42:36 +0800

    larger icon

--- components/shared/CountrySelector.tsx
+++ components/shared/CountrySelector.tsx
@@ -87,23 +87,25 @@ const CountrySelector = () => {
   };
 
   return (
-    <Select value={country} onValueChange={changeCountry}>
-      <SelectTrigger className="w-[40px] h-[40px] p-0 border-none">
-        <SelectValue>
-          {countryFlags[country]}
-        </SelectValue>
-      </SelectTrigger>
-      <SelectContent>
-        {Object.entries(countryFlags).map(([countryName, flag]) => (
-          <SelectItem key={countryName} value={countryName}>
-            <div className="flex items-center">
-              <span className="mr-2">{flag}</span>
-              {countryName}
-            </div>
-          </SelectItem>
-        ))}
-      </SelectContent>
-    </Select>
+    <div className="country-selector">
+      <Select value={country} onValueChange={changeCountry}>
+        <SelectTrigger className="w-[48px] h-[48px] p-0 border-none bg-transparent hover:bg-transparent focus:ring-0 focus:ring-offset-0">
+          <SelectValue>
+            <span className="text-3xl">{countryFlags[country]}</span>
+          </SelectValue>
+        </SelectTrigger>
+        <SelectContent>
+          {Object.entries(countryFlags).map(([countryName, flag]) => (
+            <SelectItem key={countryName} value={countryName}>
+              <div className="flex items-center">
+                <span className="text-2xl mr-2">{flag}</span>
+                {countryName}
+              </div>
+            </SelectItem>
+          ))}
+        </SelectContent>
+      </Select>
+    </div>
   );
 };
 
