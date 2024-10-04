2e0f615dcbaad52fe86e0d30551f627560a94dda
Gavin Ong <kukudoom@gmail.com>
2024-10-04 15:45:43 +0800

    icons

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -226,7 +226,7 @@ const EventForm = ({ userId, type, event, eventId }: EventFormProps) => {
                 <FormControl>
                   <div className="flex-center h-[54px] w-full overflow-hidden rounded-full bg-grey-50 px-4 py-2">
                     <Image
-                      src="/assets/icons/globe.svg"
+                      src="/assets/icons/world.svg"
                       alt="globe"
                       width={24}
                       height={24}
