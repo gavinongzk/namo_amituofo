65ca677e0d3da021ab8972eecd54fb6b18ca6821
Gavin Ong <kukudoom@gmail.com>
2024-10-14 16:54:12 +0800

    add button

--- components/shared/AdminActions.tsx
+++ components/shared/AdminActions.tsx
@@ -6,22 +6,27 @@ const AdminActions = () => {
     <div className="flex flex-col gap-4 p-6 bg-white rounded-lg shadow-md">
       <h4 className="text-lg font-semibold text-center">Admin Actions</h4>
       <Link href="/admin/select-event">
-        <Button className="w-full bg-blue-600 hover:bg-blue-700 text-white transition duration-200">
+        <Button className="w-full bg-grey-600 hover:bg-blue-700 text-white transition duration-200">
           Take Attendance
         </Button>
       </Link>
       <Link href="/admin/events_archive">
-        <Button className="w-full bg-yellow-600 hover:bg-yellow-700 text-white transition duration-200">
+        <Button className="w-full bg-black-600 hover:bg-yellow-700 text-white transition duration-200">
           Event Archive
         </Button>
       </Link>
+      <Link href="/admin/upload_orders">
+        <Button className="w-full bg-grey-600 hover:bg-yellow-700 text-white transition duration-200">
+          Upload Orders
+        </Button>
+      </Link>
       <Link href="/admin/users">
-        <Button className="w-full bg-green-600 hover:bg-green-700 text-white transition duration-200">
+        <Button className="w-full bg-black-600 hover:bg-green-700 text-white transition duration-200">
           Manage Users
         </Button>
       </Link>
       <Link href="/admin/analytics">
-        <Button className="w-full bg-purple-600 hover:bg-purple-700 text-white transition duration-200">
+        <Button className="w-full bg-grey-600 hover:bg-purple-700 text-white transition duration-200">
           Analytics
         </Button>
       </Link>
