4a3fa226043dbff76cab26888adfcc19af0f5133
Gavin Ong <kukudoom@gmail.com>
2024-10-22 23:57:10 +0800

    edit on change

--- app/(root)/admin/attendance/AttendanceClient.tsx
+++ app/(root)/admin/attendance/AttendanceClient.tsx
@@ -733,8 +733,8 @@ const AttendanceClient = React.memo(({ event }: { event: Event }) => {
                         <div className="flex flex-col sm:flex-row items-center">
                           <input
                             type="text"
-                            value={remarks[row.registrationId] || ''}
-                            onChange={(e) => setRemarks((prev) => ({ ...prev, [row.registrationId]: e.target.value }))}
+                            value={remarks[row.registrationId] || row.remarks}
+                            onChange={(e) => setRemarks((prev) => ({ ...prev, [row.registrationId]: e.target.value || '' }))}
                             className="border rounded p-1 flex-grow mb-2 sm:mb-0 sm:mr-2"
                             placeholder="Enter remarks"
                           />
