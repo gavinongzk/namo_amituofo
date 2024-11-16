bcc53ad47196ded788c11a013dc245dafb22c690
Gavin Ong <kukudoom@gmail.com>
2024-11-16 22:37:28 +0800

    more visible

--- components/shared/AnalyticsDashboard.tsx
+++ components/shared/AnalyticsDashboard.tsx
@@ -28,12 +28,16 @@ import {
   DialogHeader,
   DialogTitle,
 } from "@/components/ui/dialog"
+import { X } from 'lucide-react'
 
 ChartJS.register(CategoryScale, LinearScale, BarElement, PointElement, LineElement, Title, Tooltip, Legend);
 
 interface AttendeeEvent {
     eventDate: string;
     eventTitle: string;
+    category: {
+        name: string;
+    };
 }
 
 interface AttendeeData {
@@ -380,11 +384,18 @@ const AnalyticsDashboard: React.FC = () => {
 
             {selectedAttendee && (
                 <Dialog open={!!selectedAttendee} onOpenChange={() => setSelectedAttendee(null)}>
-                    <DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto">
-                        <DialogHeader>
+                    <DialogContent className="max-w-4xl max-h-[90vh] overflow-y-auto bg-white dark:bg-gray-900 relative">
+                        <DialogHeader className="sticky top-0 z-50 bg-white dark:bg-gray-900 pb-4 border-b">
                             <DialogTitle>User Analytics for {selectedAttendee.name}</DialogTitle>
+                            <button
+                                onClick={() => setSelectedAttendee(null)}
+                                className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-white transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-gray-400 focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-gray-100 dark:ring-offset-gray-950 dark:focus:ring-gray-800 dark:data-[state=open]:bg-gray-800"
+                            >
+                                <X className="h-4 w-4" />
+                                <span className="sr-only">Close</span>
+                            </button>
                         </DialogHeader>
-                        <div className="space-y-6 py-4">
+                        <div className="space-y-6 py-4 px-6">
                             <UserAnalyticsVisuals 
                                 attendee={selectedAttendee} 
                                 allEvents={attendees.flatMap(a => a.events)} 
