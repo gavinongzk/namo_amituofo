d77ba140a14dc88bce2d298e1e9fe35ce24334f3
Gavin Ong <kukudoom@gmail.com>
2024-12-29 21:54:43 +0800

    sentry fix

--- sentry.client.config.ts
+++ sentry.client.config.ts
@@ -4,7 +4,5 @@ Sentry.init({
   dsn: "https://99a7f55e8df103a0bd7bd9fd72c7e9db@o4508551446331392.ingest.us.sentry.io/4508551448821760",
   tracesSampleRate: 1.0,
   // Enable performance monitoring
-  integrations: [
-    new Sentry.HttpClient(),
-  ],
+  integrations: [],
 }); 
