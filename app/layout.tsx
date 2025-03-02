48ab9d8def5412009783c6bfd28d58561c59676c
Gavin Ong <kukudoom@gmail.com>
2025-03-03 00:02:54 +0800

    Remove SVG favicon link from layout
    - Remove redundant SVG favicon link from layout.tsx
    - Simplify favicon configuration in the HTML head section

--- app/layout.tsx
+++ app/layout.tsx
@@ -80,7 +80,6 @@ export default function RootLayout({
         <head>
           {preloadResources}
           <link rel="icon" href="/favicon.ico" sizes="any" />
-          <link rel="icon" href="/assets/images/logo.svg" type="image/svg+xml"/>
           <link rel="apple-touch-icon" href="/apple-touch-icon.png"/>
           <link rel="manifest" href="/manifest.webmanifest"/>
           </head>
