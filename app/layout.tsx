5e1876bc394f0d57aa1e5473f0e736af49f15246
Gavin Ong <kukudoom@gmail.com>
2025-03-07 14:57:44 +0800

    Remove Vercel Analytics from root layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,6 @@ import { Poppins } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
 import { Toaster } from 'react-hot-toast';
 import { RouteWarmer } from '@/components/shared/RouteWarmer';
-import { Analytics } from "@vercel/analytics/react"
 
 import './globals.css'
 
@@ -100,7 +99,6 @@ export default function RootLayout({
         <body className={poppins.variable}>
           <div className="context-container">
             {children}
-            <Analytics />
           </div>
           <Toaster
             position="top-center"
