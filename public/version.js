24a4867a60b65ec5c12b2e457ff0e29486f1d64e
Gavin Ong <kukudoom@gmail.com>
2025-03-18 17:20:33 +0800

    Update PostCSS configuration and versioning information
    - Changed the PostCSS configuration from CommonJS to ES module syntax for better compatibility with modern JavaScript.
    - Updated the version.js file with the latest application version and deployment timestamp to reflect recent changes.

--- public/version.js
+++ public/version.js
@@ -1,6 +1,6 @@
-// Auto-generated on: 2025-03-18T08:28:25.188Z
+// Auto-generated on: 2025-03-18T09:18:15.068Z
 // Application version number for debugging purposes
-window.APP_VERSION = '1742286505188';
+window.APP_VERSION = '1742289495067';
 
 // Deployment timestamp for debugging
-window.LAST_DEPLOY_TIME = '2025-03-18T08:28:25.189Z';
+window.LAST_DEPLOY_TIME = '2025-03-18T09:18:15.069Z';
