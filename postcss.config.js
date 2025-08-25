ccd712bda6381de320811570ddd6f9e5ef1daa8b
Gavin Ong <gavinongzk@gmail.com>
2025-08-25 20:51:53 +0800

    Update package dependencies and refactor PostCSS configuration
    - Added `tailwindcss`, `autoprefixer`, and `postcss` to `package.json` for improved styling and compatibility.
    - Refactored `postcss.config.js` to use an object format for plugins, specifying the Tailwind configuration path, enhancing clarity and maintainability.

--- postcss.config.js
+++ postcss.config.js
@@ -1,6 +1,10 @@
+const path = require('path');
+
 module.exports = {
-  plugins: [
-    require('tailwindcss'),
-    require('autoprefixer'),
-  ],
+  plugins: {
+    tailwindcss: {
+      config: path.join(__dirname, 'tailwind.config.ts'),
+    },
+    autoprefixer: {},
+  },
 }
